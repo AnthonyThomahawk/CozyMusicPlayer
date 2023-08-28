1bc2167e6f068a96d1d216efbae9343578ed62b4
bladez <antonis301@gmail.com>
2023-08-29 01:58:02 +0300

    Added media scan functionality and folder picker Also Min SDK is now 21 (Android 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.TonyTSoftware.cozymusicplayer"
-        minSdk = 19
+        minSdk = 21
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/TonyTSoftware/cozymusicplayer/MainActivity.kt
+++ app/src/main/java/com/TonyTSoftware/cozymusicplayer/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.TonyTSoftware.cozymusicplayer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.documentfile.provider.DocumentFile
+
 
 class MainActivity : AppCompatActivity() {
+    private val folderPickRequestCode = 100
+    private var audioFilesUri : ArrayList<Uri> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val selectFolderBtn : Button = findViewById(R.id.selectfolderbtn)
+        selectFolderBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+            intent.addCategory(Intent.CATEGORY_DEFAULT)
+            startActivityForResult(Intent.createChooser(intent, "Choose directory"), folderPickRequestCode);
+        }
+    }
+
+    private fun refreshTextView() {
+        val textView : TextView = findViewById(R.id.textView)
+        textView.text = "Music files currently loaded : \n"
+        var i = 1
+        for (audioFile in audioFilesUri) {
+            val audioFiledoc : DocumentFile? = DocumentFile.fromSingleUri(this, audioFile)
+            val filename = audioFiledoc?.name
+            textView.text = textView.text as String + i.toString() + ")" + filename + "\n"
+            i++
+
+            // below code returns null if no metadata exists, will keep this for later
+//            val metaDataRetriever = MediaMetadataRetriever()
+//            metaDataRetriever.setDataSource(this, audioFile)
+//            val artist : String? = metaDataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST)
+//            val title : String? = metaDataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE)
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK && requestCode == folderPickRequestCode) {
+            val selectedFolderUri = data?.data
+            if (selectedFolderUri != null) {
+                val selectedFolder : DocumentFile? = DocumentFile.fromTreeUri(this, selectedFolderUri!!)
+                val filesInFolder = selectedFolder?.listFiles()
+                if (filesInFolder != null)
+                    for (file in filesInFolder)
+                        if (file.isFile) {
+                            val fileExtension = file.name?.substringAfterLast('.', "")
+                            if (fileExtension == "mp3" || fileExtension == "ogg" || fileExtension == "flac")
+                                audioFilesUri.add(file.uri)
+                        }
+                refreshTextView()
+            }
+        }
     }
 }
